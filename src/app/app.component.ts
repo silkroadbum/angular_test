b2e5bd72bf322909966f75d81c6d66a1be958b21
silkroadbum <54482247+silkroadbum@users.noreply.github.com>
2023-09-13 23:47:07 +0600

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IProduct } from './models/product';
+import { ProductsService } from './services/products.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'angular-minin';
+export class AppComponent implements OnInit {
+  title = 'angular app'
+  products: IProduct[] = []
+  loading = false
+
+  constructor(private productsService: ProductsService) {
+  }
+
+  ngOnInit(): void {
+    this.loading = true;
+    this.productsService.getAll().subscribe((products) => {
+      this.products = products;
+      this.loading = false
+    })
+  }
 }
